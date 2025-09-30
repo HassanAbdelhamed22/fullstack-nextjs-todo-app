d375350b5d2c4f6458d3f521734e47864b0dcefe
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-09-30 08:11:28 +0300

    add faker data to database from faker-js

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.createMany({
+    data: Array.from({ length: 20 }, () => ({
+      name: faker.internet.userName(),
+      email: faker.internet.email(),
+      address: {
+        street: faker.address.street(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        zip: faker.address.zipCode(),
+      },
+    })),
+  });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
