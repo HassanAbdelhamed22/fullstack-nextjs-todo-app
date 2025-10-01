6cf69d2b80bc9af9b5438386b29d9fb6f5d32a41
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-10-01 07:08:48 +0300

    Refactor Home component: replace inline todo form with AddTodoForm component

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,8 @@
-"use client";
-
+import AddTodoForm from "@/components/AddTodoForm";
 import { ModeToggle } from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { getTodoAction } from "@/actions/todo.actions";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Textarea } from "@/components/ui/textarea";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-
-const todoFormSchema = z.object({
-  title: z
-    .string()
-    .min(5, { message: "Title must be at least 5 characters." })
-    .max(30, { message: "Title must be at most 30 characters." }),
-  body: z
-    .string()
-    .max(100, { message: "Body must be at most 100 characters." })
-    .optional(),
-});
 
 export default function Home() {
   // const todos = await getTodoAction();
-
-  type TodoFormValues = z.infer<typeof todoFormSchema>;
-  const defaultValues: Partial<TodoFormValues> = {
-    title: "Default title",
-    body: "Default body",
-  };
-
-  function onSubmit(data: TodoFormValues) {
-    console.log(data);
-  }
-
-  const form = useForm<TodoFormValues>({
-    resolver: zodResolver(todoFormSchema),
-    defaultValues,
-    mode: "onChange",
-  });
 
   return (
     <div className="container mx-auto p-4">
@@ -65,70 +10,7 @@ export default function Home() {
 
       <div className="flex justify-between items-center mb-2 mt-5">
         <h1 className="text-3xl font-bold">Todo List</h1>
-        <Dialog>
-          <form>
-            <DialogTrigger asChild>
-              <Button className="cursor-pointer">
-                <Plus />
-                New Todo
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Create todo</DialogTitle>
-                <DialogDescription>
-                  Fill the form below to create a new todo.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4">
-                <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-8"
-                  >
-                    <FormField
-                      control={form.control}
-                      name="title"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Title</FormLabel>
-                          <FormControl>
-                            <Input placeholder="Todo title" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    {/* for body */}
-                    <FormField
-                      control={form.control}
-                      name="body"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Description</FormLabel>
-                          <FormControl>
-                            <Textarea
-                              placeholder="Todo body"
-                              className="resize-none"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </form>
-                </Form>
-              </div>
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button variant="outline">Cancel</Button>
-                </DialogClose>
-                <Button type="submit">Save changes</Button>
-              </DialogFooter>
-            </DialogContent>
-          </form>
-        </Dialog>
+        <AddTodoForm />
       </div>
 
       {/* <div className="max-w-3xl mt-4">
