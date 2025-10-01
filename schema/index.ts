83c4fa22945552207af094a93595a51ca1d65aa0
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-10-01 07:08:24 +0300

    Add todo form validation schema using Zod

---
+++ schema/index.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const todoFormSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title must be at least 5 characters." })
+    .max(30, { message: "Title must be at most 30 characters." }),
+  body: z
+    .string()
+    .max(100, { message: "Body must be at most 100 characters." })
+    .optional(),
+});
+
+export type TodoFormValues = z.infer<typeof todoFormSchema>;
