f09bb97b753921ca67041c34c7991f9e34e02569
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-10-01 07:30:06 +0300

    Implement createTodoAction to handle todo creation with title and optional body

--- actions/todo.actions.ts
+++ actions/todo.actions.ts
@@ -8,6 +8,13 @@ export const getTodoAction = async () => {
   const todos = await prisma.todo.findMany();
   return todos;
 };
-export const createTodoAction = async () => {};
+export const createTodoAction = async ({ title, body}: { title: string; body?: string }) => {
+  await prisma.todo.create({
+    data: {
+      title,
+      body,
+    },
+  });
+};
 export const updateTodoAction = async () => {};
 export const deleteTodoAction = async () => {};
