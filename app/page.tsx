776c82a99dcdff1cdc59d64d06bc7841a390ef46
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-10-03 09:32:07 +0300

    Add Toaster component for improved notification display in Home

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import { getTodoAction } from "@/actions/todo.actions";
 import AddTodoForm from "@/components/AddTodoForm";
 import { ModeToggle } from "@/components/ModeToggle";
 import { TodoTable } from "@/components/TodoTable";
+import { Toaster } from "@/components/ui/sonner";
 
 export default async function Home() {
   const todos = await getTodoAction();
 
   return (
     <div className="container max-w-6xl mx-auto p-4">
+      <Toaster position="top-right" richColors />
+
       {/* Header */}
       <div className="flex justify-between items-center py-3 border-b mb-6">
         <h1 className="text-3xl font-bold tracking-tight">Todo List</h1>
