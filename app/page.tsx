de5fbd8e57ddb4152b925eee4edfdcc015c26bf8
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-10-07 08:51:11 +0300

    retrieve userId from auth() using clerk/nextjs/server and sent it as a prop to AddTodoForm component

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import AddTodoForm from "@/components/AddTodoForm";
 import { ModeToggle } from "@/components/ModeToggle";
 import { TodoTable } from "@/components/TodoTable";
 import { Toaster } from "@/components/ui/sonner";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function Home() {
   const todos = await getTodoAction();
+  const { userId } = await auth();
 
   return (
     <div>
@@ -14,7 +16,7 @@ export default async function Home() {
       {/* Add Form */}
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-semibold">Manage your tasks</h2>
-        <AddTodoForm />
+        <AddTodoForm userId={userId} />
       </div>
 
       {/* Todo Section */}
